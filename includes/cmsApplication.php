ad742608e5f6f1786e3980439d5294a3371e6876
unknown <vengeancedonny@gmail.com>
2015-03-28 15:57:33 +0700

    perbaikan pertama mycms

---
+++ includes/cmsApplication.php
@@ -0,0 +1,5 @@
+<?php
+require_once('cmsBase.php');
+class cmsApplication extends cmsBase {
+	// semua kode yang ada disini dapat diakses melalui fungsi utama dalam CMS
+	}
---
+++ includes/cmsWidget.php
@@ -0,0 +1,4 @@
+<?php
+require_once('cmsBase.php');
+	// semua kode yang ada disini dapat diakses melalui fungsi Widget dalam CMS
+	}
